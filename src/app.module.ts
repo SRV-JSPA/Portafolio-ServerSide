293df6272608f823080f56371434d6ca8f94d251
JSPA <per22318@uvg.edu.gt>
2024-05-20 08:44:38 -0600

    Created module education

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EducationModule } from './education/education.module';
 
 @Module({
-  imports: [],
+  imports: [EducationModule],
   controllers: [AppController],
   providers: [AppService],
 })
