3a56b78089ec604b1ecd014ca49002baabedbe4b
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-05-21 08:14:38 -0600

    Education entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EducationModule } from './education/education.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
 
 @Module({
-  imports: [EducationModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 33069,
+    username: 'root',
+    password: 'pereira',
+    database: 'portafolio',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  }),EducationModule],
   controllers: [AppController],
   providers: [AppService],
 })
